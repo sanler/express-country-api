6e451adc5e98cae1f67ba863a9f9a62ec3d98c7f
sanler89 <sanler110489@gmail.com>
2019-06-05 12:34:41 +0200

    first commit

--- app.js
+++ app.js
@@ -2,16 +2,50 @@ const express = require('express')
 const app     = express()
 const hbs     = require('hbs') 
 const path    = require('path');
-
+const countries = require('./countries.json');
+console.log(countries);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-  console.log(req);
-})
 
-app.get('/users/:username', (req, res, next) => {
-    res.send(req.params);
-  })
+app.get('/', (req, res, next) => {
+
+  res.render('index');
+
+});
+
+
+app.get('/countries', (req, res, next) => {
+
+
+
+
+  res.send(countries);
+
+});
+
+
+app.get('/countries/:code', (req, res, next) => {
+ 
+  const code=req.params.code;
+  const getCountry = (code) => {
+    const theCountry = oneCountry => {
+      return oneCountry.cca3 === code;
+    }
+    return countries.find(theCountry)
+  };
+  const foundCountry = getCountry(code,10);
+
+    res.send(foundCountry);
+  
+  });
+
+app.get('/search', (req, res, next) => {
+    
+  
+  res.send(req.query);
+
+
+  });
 
 app.listen(3000, () => console.log('App listening on port 3000!'))
